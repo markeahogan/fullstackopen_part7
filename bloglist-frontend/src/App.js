596a9c8ab641e7f1aaf5a0ac5f39f114b6bc224b
Mark Hogan <markeahogan@gmail.com>
2019-12-10 08:42:37 +0000

    Start on routing

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link, withRouter } from 'react-router-dom';
 
 import LoginForm from './components/LoginForm';
 import BlogsList from './components/BlogsList';
@@ -6,7 +7,6 @@ import Togglable from './components/Togglable';
 import UserDetails from './components/UserDetails';
 import CreateBlogForm from './components/CreateBlogForm';
 import Notification from './components/Notification';
-
 import { getAll } from './reducers/blogsReducer';
 import { tryLoginWithLocalStorage } from './reducers/userReducer';
 
@@ -20,21 +20,28 @@ function App() {
         tryLoginWithLocalStorage();
     }, []);
 
-    return (
+    const BlogsPage = () => {
+        return (
+            <Togglable buttonLabel = {'Create blog'} >
+                <CreateBlogForm />
+            </Togglable>
+            <BlogsList />
+        );
+    };
 
-        <div className="App">
-            <Notification />
-            {user===null && <LoginForm />}
-            {user!==null &&
-      (<>
-        <UserDetails />
-        <Togglable buttonLabel = {'Create blog'} >
-            <CreateBlogForm />
-        </Togglable>
-        <BlogsList />
-        </>)
-            }
-        </div>
+    return (
+        <Router>
+            <div className="App">
+                <Notification />
+                {user===null && <LoginForm />}
+                {user!==null && (
+                <>
+                    <UserDetails />
+                    <Route exact path='/' render={BlogsPage} />   
+                </>
+                )}
+            </div>
+        </Router>
     );
 }
 
