4fd0f6c841701886d0d4c24ca6ea9b101f1cf492
Mark Hogan <markeahogan@gmail.com>
2019-12-12 13:22:14 +0000

    start on blogs as links

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { like, remove } from '../reducers/blogsReducer';
 
 const Blog = ({ blog, like, remove, currentUser }) => {
-
-    const [expanded, setExpanded] = useState(false);    
 
     const blogStyle = {
         paddingTop: 10,
@@ -13,22 +12,10 @@ const Blog = ({ blog, like, remove, currentUser }) => {
         borderWidth: 1,
         marginBottom: 5
     };
-
-    const show = { display: '' };
-    const hide = { display: 'none' };
-
-    const username = (blog.user && blog.user.name) || '';
-    const showRemoveButton = !blog.user || currentUser && blog.user.username === currentUser.username;
-
+    
     return (
-        <div style={blogStyle} onClick={() => setExpanded(!expanded)}>
-            {blog.title} {blog.author}
-            <div className={'expandable'} style={expanded ? show : hide}>
-                <div>{blog.url}</div>
-                <div>{blog.likes} likes <button onClick={(e) => {like(blog); e.stopPropagation();}}>like</button></div>
-                <div>added by {username}</div>
-                {showRemoveButton && (<button onClick={(e) => {remove(blog); e.stopPropagation();}}>remove</button>)}
-            </div>
+        <div style={blogStyle}>
+        <Link to={`/${blog.id}`}>{blog.title}</Link>
         </div>
     );
 };
--- bloglist-frontend/src/components/SingleBlogPage.js
+++ bloglist-frontend/src/components/SingleBlogPage.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { like } from '../reducers/blogsReducer';
+import { like, remove } from '../reducers/blogsReducer';
 
-const SingleBlogPage = ({blog, like}) => {
+const SingleBlogPage = ({blog, deletable, like, remove}) => {
     return (
         <>
             <h1>{blog.title}</h1>
             <Link to={blog.url}>{blog.url}</Link>
             <div>{blog.likes} likes <button onClick={()=>like(blog)}>like</button></div>
             <div>by {blog.author}</div>
+            {deletable && <button onClick={() => remove(blog)}>remove</button>}
         </>
     )
 }
 
 const mapStateToProps = (state, {blogID}) => {
+    const blog = state.blogs.find(x => x.id === blogID);
+    const deletable = state.users.current.id === blog.user.id;
     return {
-        blog:state.blogs.find(x => x.id === blogID);
+        blog,
+        deletable
     };
 };
 
 const mapDispatchToProps = {
-    like
+    like,
+    remove
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(SingleBlogPage);
