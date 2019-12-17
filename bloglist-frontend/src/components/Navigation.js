36d63f8c5d75143d1b8b843ccad2155990063c99
Mark Hogan <mark@popupasylum.co.uk>
2019-12-17 22:34:00 +0000

    Navigation styling

--- bloglist-frontend/src/components/Navigation.js
+++ bloglist-frontend/src/components/Navigation.js
@@ -2,13 +2,35 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import {logout} from '../reducers/userReducer';
+import {Navbar, Nav} from 'react-bootstrap';
 
 const Navigation = ({user, logout}) => {
+
+    const padding = {
+        paddingTop: 10,
+        paddingLeft: 2
+    };
+
 return(
-    <>
-        <Link to='/'>blogs</Link> <Link to='/users'>users</Link> {user.name} logged in <button onClick={logout}>log out</button>
-    </>
-    );
+<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#" as="span">
+        <Link style={padding} to="/">Blogs</Link>
+      </Nav.Link>
+      <Nav.Link href="#" as="span">
+        <Link style={padding} to="/users">Users</Link>
+      </Nav.Link>
+      <Nav.Link href="#" as="span">
+        {user
+          ? <em>{user.name} logged in</em>
+          : <Link to="/login">login</Link>
+        }
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>)
 }
 
 const mapStateToProps = state => {
